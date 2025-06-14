6f1e5047490287124e97d7d60525eb93b335b081
Mateus <mateusmachry2016@gmail.com>
2025-06-14 18:35:38 -0300

    fixed exports

--- src/wheel-of-fortune/index.ts
+++ src/wheel-of-fortune/index.ts
@@ -1,9 +1,12 @@
-import type { WheelOfFortune, WheelOfFortuneProps, WheelOfFortuneRef } from "./components/wheel-of-fortune";
+import { WheelOfFortune } from "./components/wheel-of-fortune";
+import type { WheelOfFortuneProps, WheelOfFortuneRef } from "./components/wheel-of-fortune";
 import type { WheelOfFortunePrize } from "./types/wheel-of-fortune-prize";
 
 export type {
     WheelOfFortuneProps,
     WheelOfFortuneRef,
-    WheelOfFortune,
     WheelOfFortunePrize,
+}
+export {
+    WheelOfFortune,
 }
